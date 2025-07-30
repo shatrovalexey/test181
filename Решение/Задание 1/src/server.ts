204d54d0505862f5ab9d24f5bce4ee9833425179
Алексей <mail@ashatrov.ru>
2025-07-30 18:38:35 +0300

    Initial

--- Решение/Задание 1/src/server.ts
+++ Решение/Задание 1/src/server.ts
@@ -15,35 +15,51 @@ fastify.register(cors, {
 
 const dataPath = path.join(__dirname, 'data', 'users.json');
 
-fastify.get('/', (req: FastifyRequest<SearchRoute>, res) => {
-	fs.readFile(dataPath, 'utf8', (err, data) => {
-		try {
-			if (err) {
-				fastify.log.error('File read failed: ' + err);
-				return res.code(500).send(errorRes("Server error"));
-			}
+fastify.get('/', (req: FastifyRequest<SearchRoute>, res) => fs.readFile("web/index.html", "utf8", (err, data) => {
+    return res.type('text/html').send(data);
+}));
 
-			const users: User[] = JSON.parse(data);
-			
-			const result = users
-				.filter((elem) => elem.name.toLowerCase()
-				.search(req.query.term?.toLowerCase() ?? '') !== -1);
-	
-			return res.type('application/json').send(successRes<User[]>(result));
-		} catch(e) {
-			fastify.log.error(e);
-			return res.status(500).send(errorRes("Server error"))
-		}
-	})
+[
+    ["/js", "js/script.js", "application/javascript",]
+    , ["/css", "css/style.css", "text/css",]
+    , ["/font/regular", "font/ProximaNova/proximanova_regular.ttf", "font/ttf",]
+    , ["/font/bold", "font/ProximaNova/proximanova_bold.otf", "font/otf",]
+    , ["/img/email", "img/email.png", "image/x-png",]
+    , ["/img/phone", "img/phone.png", "image/x-png",]
+    , ["/img/zoom", "img/zoom.png", "image/x-png",]
+    ,
+].forEach(([route, path, mime,]) => fastify.get(route
+    , (req: FastifyRequest<SearchRoute>, res) => res.type(mime).send(fs.readFileSync(`web/${path}`))));
+
+fastify.get('/users', (req: FastifyRequest<SearchRoute>, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+        try {
+            if (err) {
+                fastify.log.error('File read failed: ' + err);
+                return res.code(500).send(errorRes("Server error"));
+            }
+
+            const users: User[] = JSON.parse(data);
+            
+            const result = users
+                .filter((elem) => elem.name.toLowerCase()
+                .search(req.query.term?.toLowerCase() ?? '') !== -1);
+    
+            return res.type('application/json').send(successRes<User[]>(result));
+        } catch(e) {
+            fastify.log.error(e);
+            return res.status(500).send(errorRes("Server error"))
+        }
+    })
 });
 
 const start = async () => {
-	try {
-		await fastify.listen({port: PORT, host: HOST})
-	} catch (err) {
-		fastify.log.error(err);
-		process.exit(1);
-	}
+    try {
+        await fastify.listen({port: PORT, host: HOST})
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1);
+    }
 };
 
 start();

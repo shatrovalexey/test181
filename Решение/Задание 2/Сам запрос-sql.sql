ff95c91e1571b0ccd0ce4fcc581a89cf5433f33f
Алексей <mail@ashatrov.ru>
2025-07-31 09:08:27 +0300

    Задание №2

--- Решение/Задание 2/Сам запрос-sql.sql
+++ Решение/Задание 2/Сам запрос-sql.sql
@@ -6,9 +6,9 @@ WITH [subdivision_subtree] AS (
         [s1].[id]
         , [s1].[parent_id]
     FROM
-        [test181].[dbo].[subdivisions] AS [s1]
+        [dbo].[subdivisions] AS [s1]
 
-            INNER JOIN [test181].[dbo].[collaborators]  AS [c1]
+            INNER JOIN [dbo].[collaborators]  AS [c1]
                 ON ([s1].[id] = [c1].[subdivision_id])
     WHERE
         ([c1].[id] = @collaborator_id)
@@ -19,7 +19,7 @@ WITH [subdivision_subtree] AS (
         [s1].[id]
         , [s1].[parent_id]
     FROM
-        [test181].[dbo].[subdivisions] AS [s1]
+        [dbo].[subdivisions] AS [s1]
 
             INNER JOIN [subdivision_subtree] AS [sd1]
                 ON ([s1].[parent_id] = [sd1].[id])
@@ -41,44 +41,46 @@ WITH [subdivision_subtree] AS (
         , [s1].[parent_id]
         , [sdl1].[level] + 1 AS [level]
     FROM
-        [test181].[dbo].[subdivisions] AS [s1]
+        [dbo].[subdivisions] AS [s1]
 
             INNER JOIN [subdivision_tree_level] AS [sdl1]
                 ON ([s1].[parent_id] = [sdl1].[id])
 )
 , [subdivision_colls_count] AS (
     SELECT 
-        [c1].[subdivision_id] AS [id]
-        , count(*) AS [count]
+        [stl1].[id]
+        , [stl1].[level]
+        , count([c1].[id]) AS [count]
     FROM
-        [test181].[dbo].[collaborators] AS [c1]
+        [subdivision_tree_level] AS [stl1]
+
+            LEFT OUTER JOIN [dbo].[collaborators] AS [c1]
+                ON ([c1].[subdivision_id] = [stl1].[id])
     GROUP BY
-        [c1].[subdivision_id]
+        [stl1].[id]
+        , [stl1].[level]
 )
 SELECT
     [c1].[id]
     , [c1].[name]
     , [s1].[name] AS [sub_name]
-    , [s1].[id] AS [sub_id]
-    , [sdl1].[level] AS [sub_level]
+    , [cc1].[id] AS [sub_id]
+    , [cc1].[level] AS [sub_level]
     , [cc1].[count] AS [colls_count]
 FROM
-    [test181].[dbo].[collaborators] AS [c1]
+    [dbo].[collaborators] AS [c1]
 
-        INNER JOIN [test181].[dbo].[subdivisions] AS [s1]
+        INNER JOIN [dbo].[subdivisions] AS [s1]
             ON ([s1].[id] = [c1].[subdivision_id])
 
         INNER JOIN [subdivision_subtree] AS [sd1]
             ON ([c1].[subdivision_id] = [sd1].[id])
 
+        INNER JOIN [dbo].[collaborators] AS [c2]
+            ON ([s1].[id] <> [c2].[subdivision_id])
+
         INNER JOIN [subdivision_colls_count] AS [cc1]
             ON ([sd1].[id] = [cc1].[id])
-
-        INNER JOIN [subdivision_tree_level] AS [sdl1]
-            ON ([sdl1].[id] = [s1].[id])
-
-        INNER JOIN [test181].[dbo].[collaborators] AS [c2]
-            ON ([c1].[subdivision_id] <> [c2].[subdivision_id])
 WHERE
     ([c1].[age] < @collaborator_age_limit)
         AND ([c2].[id] = @collaborator_id)

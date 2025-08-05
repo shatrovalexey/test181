c672a55f843838a83e5dc8f6f1aef0ce8ea1d90c
Алексей <mail@ashatrov.ru>
2025-08-05 07:02:50 +0300

    Задание 2

--- Решение/Задание 2/Сам запрос-sql.sql
+++ Решение/Задание 2/Сам запрос-sql.sql
@@ -2,65 +2,65 @@ DECLARE @collaborator_id BIGINT = 710253
 DECLARE @collaborator_age_limit BIGINT = 40;
 
 WITH
-	[subdivision_subtree] AS (
-		SELECT
-			[s1].[id]
-			, [s1].[parent_id]
-		FROM
-			[dbo].[subdivisions] AS [s1]
+    [subdivision_subtree] AS (
+        SELECT
+            [s1].[id]
+            , [s1].[parent_id]
+        FROM
+            [dbo].[subdivisions] AS [s1]
 
-				INNER JOIN [dbo].[collaborators]  AS [c1]
-					ON ([s1].[id] = [c1].[subdivision_id])
-		WHERE
-			([c1].[id] = @collaborator_id)
-		
-		UNION ALL
+                INNER JOIN [dbo].[collaborators]  AS [c1]
+                    ON ([s1].[id] = [c1].[subdivision_id])
+        WHERE
+            ([c1].[id] = @collaborator_id)
+        
+        UNION ALL
 
-		SELECT
-			[s1].[id]
-			, [s1].[parent_id]
-		FROM
-			[dbo].[subdivisions] AS [s1]
+        SELECT
+            [s1].[id]
+            , [s1].[parent_id]
+        FROM
+            [dbo].[subdivisions] AS [s1]
 
-				INNER JOIN [subdivision_subtree] AS [sd1]
-					ON ([s1].[parent_id] = [sd1].[id])
-	)
-	, [subdivision_tree_level] AS (
-		SELECT
-			[s1].[id]
-			, [s1].[parent_id]
-			, 0 AS [level]
-		FROM
-			[dbo].[subdivisions] AS [s1]
-		WHERE
-			([s1].[parent_id] IS null)
+                INNER JOIN [subdivision_subtree] AS [sd1]
+                    ON ([s1].[parent_id] = [sd1].[id])
+    )
+    , [subdivision_tree_level] AS (
+        SELECT
+            [s1].[id]
+            , [s1].[parent_id]
+            , 0 AS [level]
+        FROM
+            [dbo].[subdivisions] AS [s1]
+        WHERE
+            ([s1].[parent_id] IS null)
 
-		UNION ALL
+        UNION ALL
 
-		SELECT
-			[s1].[id]
-			, [s1].[parent_id]
-			, [sdl1].[level] + 1 AS [level]
-		FROM
-			[dbo].[subdivisions] AS [s1]
+        SELECT
+            [s1].[id]
+            , [s1].[parent_id]
+            , [sdl1].[level] + 1 AS [level]
+        FROM
+            [dbo].[subdivisions] AS [s1]
 
-				INNER JOIN [subdivision_tree_level] AS [sdl1]
-					ON ([s1].[parent_id] = [sdl1].[id])
-	)
-	, [subdivision_colls_count] AS (
-		SELECT 
-			[stl1].[id]
-			, [stl1].[level]
-			, count([c1].[id]) AS [count]
-		FROM
-			[subdivision_tree_level] AS [stl1]
+                INNER JOIN [subdivision_tree_level] AS [sdl1]
+                    ON ([s1].[parent_id] = [sdl1].[id])
+    )
+    , [subdivision_colls_count] AS (
+        SELECT 
+            [stl1].[id]
+            , [stl1].[level]
+            , count([c1].[id]) AS [count]
+        FROM
+            [subdivision_tree_level] AS [stl1]
 
-				LEFT OUTER JOIN [dbo].[collaborators] AS [c1]
-					ON ([c1].[subdivision_id] = [stl1].[id])
-		GROUP BY
-			[stl1].[id]
-			, [stl1].[level]
-	)
+                LEFT OUTER JOIN [dbo].[collaborators] AS [c1]
+                    ON ([c1].[subdivision_id] = [stl1].[id])
+        GROUP BY
+            [stl1].[id]
+            , [stl1].[level]
+    )
 SELECT
     [c1].[id]
     , [c1].[name]
@@ -81,8 +81,9 @@ FROM
             ON ([sd1].[id] = [cc1].[id])
 
         LEFT OUTER JOIN [dbo].[collaborators] AS [c2]
-            ON ([s1].[id] <> [c2].[subdivision_id])
+            ON ([s1].[id] = [c2].[subdivision_id])
+                AND ([c2].[id] = @collaborator_id)
 WHERE
     ([c1].[age] < @collaborator_age_limit)
-        AND ([c2].[id] = @collaborator_id)
+        AND ([c2].[id] IS null)
         AND ([c1].[id] NOT IN (100055, 100059));
